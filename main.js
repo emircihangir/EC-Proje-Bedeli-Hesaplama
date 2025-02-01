6bab135da0c7e6785c7d7640dafd52fb6854f867
Muhammed Emir Cihangir <69801024+emircihangir@users.noreply.github.com>
2025-02-01 16:42:14 +0300

    Finished tab2

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 let uygulama_sayisi = 1;
+let uygulama_sayisi_tab2 = 1;
 
 fetch("./data/tablo8.json").then((response) => response.json()).then((tablo8_data) => {
     retrive_msInput_options(tablo8_data);
@@ -31,19 +32,33 @@ function msInput_selected(selected_value) {
     });
 }
 
-function increase_us() {
-    uygulama_sayisi += 1;
-    document.getElementById("usInput").value = uygulama_sayisi.toString() + ". Uygulama";
+function increase_us(is_tab2 = false) {
+    if(is_tab2){
+        uygulama_sayisi_tab2 += 1;
+        document.getElementById("usInput_tab2").value = uygulama_sayisi_tab2.toString() + ". Uygulama";
+    }
+    else{
+        uygulama_sayisi += 1;
+        document.getElementById("usInput").value = uygulama_sayisi.toString() + ". Uygulama";
+    }
 }
 
-function decrease_us() {
-    if (uygulama_sayisi == 1) return;
+function decrease_us(is_tab2 = false) {
+    if(is_tab2){
+        if (uygulama_sayisi_tab2 == 1) return;
 
-    uygulama_sayisi -= 1;
-    document.getElementById("usInput").value = uygulama_sayisi.toString() + ". Uygulama";
+        uygulama_sayisi_tab2 -= 1;
+        document.getElementById("usInput_tab2").value = uygulama_sayisi_tab2.toString() + ". Uygulama";
+    }
+    else{
+        if (uygulama_sayisi == 1) return;
+
+        uygulama_sayisi -= 1;
+        document.getElementById("usInput").value = uygulama_sayisi.toString() + ". Uygulama";
+    }    
 }
 
-async function calculate() {
+async function tab1_calculate() {
     let should_calculate = true;
     let yaInput = document.getElementById("yaInput");
     let msInput = document.getElementById("msInput");
@@ -54,15 +69,18 @@ async function calculate() {
 
     let inputs = [yaInput, msInput, pubInput, ygInput, tsInput, temelInput];
 
+    let YSK, BM, PUO, PYK, HB, BK;
+    // YSK : Yapı Sınıfı Katsayısı
+    // BM : Birim Maliyet
+    // PUO : Proje Ücret Oranı
+    // IMHO : İnşaat Mühendisliği Hizmet Oranı
+    // PYK : Proje Yineleme Katsayısı
+    // HB : Hizmet Bölümleri
+    // BK : Bölge Katsayısı
+
     let ysk_puani = 0;
-    let YSK; // Yapı Sınıfı Katsayısı
     let YA = parseInt(yaInput.value); // Yapı Alanı
-    let BM; // Birim Maliyet
-    let PUO; // Proje Ücret Oranı
-    let IMHO = 0.75; // İnşaat Mühendisliği Hizmet Oranı
-    let PYK; // Proje Yineleme Katsayısı
-    let HB; // Hizmet Bölümleri
-    let BK; // Bölge Katsayısı
+    let IMHO = 0.75;
 
     //* Highlight the input as red if empty.
     for (let index = 0; index < inputs.length; index++) {
@@ -75,7 +93,7 @@ async function calculate() {
         else input_element.classList.remove("border-3", "border-danger");
     }
 
-    if (Number.isNaN(YA)) {
+    if (Number.isNaN(YA) || YA < 0) {
         yaInput.classList.add("border-3", "border-danger");
         should_calculate = false;
     }
@@ -183,9 +201,130 @@ async function calculate() {
     }
 }
 
+async function tab2_calculate(){
+    let YA, BM, HBK, PYK;
+    // YA : Yapı Alanı
+    // BM : Birim Maliyet
+    // HBK : Hizmet Bedeli Katsayısı
+    // PYK : Proje Yineleme Katsayısı
+
+    let should_calculate = true;
+    let yaInput = document.getElementById("yaInput_tab2");
+    let ygInput = document.getElementById("ygInput_tab2");
+
+    YA = parseInt(yaInput.value);
+    if (Number.isNaN(YA) || YA < 0 || YA > 100000) {
+        yaInput.classList.add("border-3", "border-danger");
+        should_calculate = false;
+    }
+    else {
+        yaInput.classList.remove("border-3", "border-danger");
+    }
+
+    if (ygInput.value == "empty") {
+        ygInput.classList.add("border-3", "border-danger");
+        should_calculate = false;
+    }
+    else ygInput.classList.remove("border-3", "border-danger");
+
+    if(should_calculate){
+        //* Calculate BM
+        let ygInput_0 = ygInput.value[0];
+        let ygInput_1 = ygInput.value[1];
+        await fetch("./data/tablo1.json").then((response) => response.json()).then((tablo1_data) => {
+            BM = tablo1_data[ygInput_0 + ". sinif yapilar"][ygInput_1];
+        });
+
+        //* Calculate HBK
+        await fetch("./data/hizmet-bedeli-cetveli.csv").then((response) => response.text()).then((d) => {
+            data = d.split("\n");
+            data.shift();
+
+            let existing_YA_values = [];
+            for (let index = 0; index < data.length; index++) {
+                const row = data[index].split(",");
+                existing_YA_values.push(parseInt(row[0]));
+            }
+
+            if (YA <= 600) {
+                const reference_row = data[0].split(",");
+                HBK = parseFloat(reference_row[1]) / 1000;
+            }
+            else {
+                if(existing_YA_values.includes(YA)){ // no need for interpolation, data already exists.
+                    for (let index = 0; index < data.length; index++) {
+                        const row = data[index].split(",");
+                        if (parseInt(row[0]) == YA) {
+                            const reference_row = data[index].split(",");
+                            HBK = parseFloat(reference_row[1]) / 1000;
+                            break;
+                        }
+                    }
+                }
+                else{ // use linear interpolation
+                    for (let index = 0; index < data.length; index++) {
+                        const row = data[index].split(",");
+    
+                        if (parseInt(row[0]) > YA) {
+                            const reference_row1 = data[index - 1].split(",");
+                            const reference_row2 = data[index].split(",");
+
+                            HBK = (linear_interpolation(
+                                [parseInt(reference_row1[0]),parseFloat(reference_row1[1])],
+                                [parseInt(reference_row2[0]),parseFloat(reference_row2[1])],
+                                YA
+                            )) / 1000;
+                            break;
+                        }
+                    }
+                }
+            }
+        });
+
+        //* Calculate PYK
+        await fetch("./data/tablo5.json").then((response) => response.json()).then((tablo5_data) => {
+            let result = 0;
+            for (let index = 1; index <= uygulama_sayisi_tab2; index++) {
+                let k;
+                if (index <= 4) k = index.toString();
+                else k = ">4";
+
+                result += tablo5_data[k];
+            }
+            PYK = parseFloat(result.toPrecision(4));
+        });
+
+        let result = YA * BM * HBK * PYK;
+        let _result = new Intl.NumberFormat('tr-TR', { style: 'currency', currency: 'TRY' }).format(result);
+        // result = parseFloat(result.toPrecision(3));
+
+        //* Log the calculation to the console
+        const debug_log = {
+            "HBK" : HBK,
+            "BM": BM,
+            "PYK": PYK,
+        };
+        console.table(debug_log);
+        console.info(_result);
+
+        //* Display the result on UI
+        document.getElementById("resultDiv_tab2").replaceChildren();
+        let p = document.createElement("p");
+        p.innerHTML = "Mesleki Denetim Hizmet Bedeli Ücreti: " + _result;
+        p.className = "text-center mt-4 fs-5";
+        document.getElementById("resultDiv_tab2").appendChild(p);
+
+        window.scrollTo({
+            top: document.body.scrollHeight,
+            behavior: "smooth"
+          });
+    }
+}
+
 async function display_result(result){
     //* If the project type is "yeni proje", a table will be displayed.
     //* Otherwise, just a text will be displayed.
+    //* This is why the if statement is needed.
 
     document.getElementById("resultDiv").replaceChildren();
 
@@ -270,4 +409,17 @@ function deselect_checkboxes() {
         const radio_element = document.querySelectorAll("input[type='checkbox']")[index];
         radio_element.checked = false;
     }
+}
+
+/**
+ * point1 = [x1, y1]
+ * 
+ * point2 = [x2, y2]
+ * 
+ * x = the x value between x1 and x2
+ */
+function linear_interpolation(point1, point2, x){
+    let diff_x = point1[0] - point2[0];
+    let diff_y = point1[1] - point2[1];
+    return point1[1] + ((x - point1[0]) * diff_y / diff_x);
 }
